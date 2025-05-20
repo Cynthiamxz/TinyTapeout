7fb3ae67169168426b2f3ff33a7f32ae41c2ea5b
Cynthia <c29ma@uwaterloo.ca>
2025-05-20 19:30:48 -0400

    8-bit counter

--- src/project.v
+++ src/project.v
@@ -17,11 +17,33 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+
+  // 8-bit counter
+  reg [7:0] counter;
+  wire load           = ui_in[7];
+  wire enable         = ui_in[6];
+  wire dir_up         = ui_in[5];
+  wire output_en      = ui_in[4];
+  wire [3:0] load_val = ui_in[3:0];
+
+  assign uio_oe = output_en ? 8'b11111111 : 8'b0; // Enable the output if output_en is high
+  assign uio_out = output_en ? counter : 8'bZ; // Output the counter value if output_en is high
+  assign uo_out  = counter;
+
+  always @(posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
+      counter <= 8'b0;
+    end else if (load) begin 
+      counter <= { 4'b0000, load_val }; // Load value from ui_in
+    end else if (enable) begin
+      if (dir_up)
+        counter <= counter + 1;
+      else
+        counter <= counter - 1;
+    end
+  end
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, uio_in, 1'b0};
 
 endmodule
--- test/test.py
+++ test/test.py
@@ -23,18 +23,51 @@ async def test_project(dut):
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
 
+    # print("+++++++++++++" + str(dut.uo_out.value) + "+++++++++++++")
+    # dut.ui_in.value = 0b01110000
+    # await ClockCycles(dut.clk, 1)
+    # print("+++++++++++++" + str(dut.uo_out.value) + "+++++++++++++")
+    # await ClockCycles(dut.clk, 1)
+    # print("+++++++++++++" + str(dut.uo_out.value) + "+++++++++++++")
+    # await ClockCycles(dut.clk, 1)
+    # print("+++++++++++++" + str(dut.uo_out.value) + "+++++++++++++")
+    # await ClockCycles(dut.clk, 1)
+    # print("+++++++++++++" + str(dut.uo_out.value) + "+++++++++++++")
+    # await ClockCycles(dut.clk, 1)
+    # print("+++++++++++++" + str(dut.uo_out.value) + "+++++++++++++")
+
     dut._log.info("Test project behavior")
 
     # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
 
-    # Wait for one clock cycle to see the output values
+    # Wait for one clock cycle to see the outpuomgomgt values
     await ClockCycles(dut.clk, 1)
 
     # The following assersion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+    assert dut.uo_out.value == 0, "Counter should be 0 after reset"
 
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
+
+    # # # Load 5 into the counter
+    dut._log.info("Load 5 into the counter")
+    dut.ui_in.value = 0b10010101
+    await ClockCycles(dut.clk, 2) # have to wait for an extra clock cycle to get correct value from non-blocking assignment
+    assert dut.uo_out.value == 5, "Counter value should be 5 after loading"
+
+    # Counter up for 3 cycles
+    dut._log.info("Count up for 3 cycles")
+    dut.ui_in.value = 0b01110000
+    await ClockCycles(dut.clk, 3)
+    dut.ui_in.value = 0b00110000
+    await ClockCycles(dut.clk, 1)
+    assert dut.uo_out.value == 8, "Counter value should be 8 after 3 counts up"
+
+    # Counter down for 2 cycles
+    dut._log.info("Count down for 2 cycles")
+    dut.ui_in.value = 0b01010000
+    await ClockCycles(dut.clk, 2)
+    dut.ui_in.value = 0b00010000
+    await ClockCycles(dut.clk, 1)
+    assert dut.uo_out.value == 6, "Counter value should be 6 after 2 counts down"
